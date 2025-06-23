dd7aa918bb3b685f379ee38b3fff94c594627fc0
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2025-06-23 14:40:01 +0300

    init

--- frontend/medi-hist-portal/src/app/page.tsx
+++ frontend/medi-hist-portal/src/app/page.tsx
@@ -1,9 +1,19 @@
-import Home from "@/app/component/home";
+'use client';
 
+import {useState} from "react";
 
 function App() {
+    const [counter,setCounter] = useState<number>(0);
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+    }
     return (
-        <Home/>
+        <div>
+            <h1>Page is working..</h1>
+            <div>Count : {counter}</div>
+            <button className="classic-link" onClick={handleClick} >Click to Increment</button>
+        </div>
     )
 }
 
