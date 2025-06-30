c2a86f2e0bc98ee03f3672cec300cf8708eb83be
m.lewes@egyptianbanks.net <minaanwer@Minas-MacBook-Pro.local>
2025-06-30 19:00:40 +0300

    update demos

--- frontend/medi-hist-portal/src/app/demo3/todo/page.tsx
+++ frontend/medi-hist-portal/src/app/demo3/todo/page.tsx
@@ -4,8 +4,6 @@ import { Todo, TodoStatus } from "./types";
 
 function ToDoPage() {
     const todos: Todo[] = getDummyTodos()
-
-
     return (
         <div className="max-w-8xl mx-auto p-12">
             <h1 className="text-2xl font-bold mb-4 text-center">..... My Todo List .....</h1>
