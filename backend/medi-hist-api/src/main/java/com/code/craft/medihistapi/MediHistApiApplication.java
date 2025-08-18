496772f0770d0e777c03f88327c5efed24418dc6
m.lewes@egyptianbanks.net <minaanwer@EBCs-MacBook-Pro.local>
2025-08-18 19:21:06 +0300

    add application properties and main application class; configure Ollama service and initialize MediHist API

---
+++ backend/medi-hist-api/src/main/java/com/code/craft/medihistapi/MediHistApiApplication.java
@@ -0,0 +1,13 @@
+package com.code.craft.medihistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediHistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediHistApiApplication.class, args);
+    }
+
+}
--- backend/medi-hist-api/src/main/java/com/code/craft/medihistapi/chat/ChatController.java
+++ backend/medi-hist-api/src/main/java/com/code/craft/medihistapi/chat/ChatController.java
@@ -2,13 +2,9 @@ package com.code.craft.medihistapi.chat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 
 @RestController
